e02b9fcd61c7cb5295a8bd5818d6829fe7d31b5b
Vasiliy <60792301+VasiliyBologov@users.noreply.github.com>
2022-02-09 00:07:48 +0200

    add templates, add server, add requirements.txt

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
-vvv
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return flask.render_template('home_page.html')
+
+@app.route('/add-book')
+def add_book():
+    return flask.render_template('add_book.html')
+
+@app.route('/books')
+def books():
+    return flask.render_template('books.html')
+
+
+if name == 'main':
+    app.run()
