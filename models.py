2ad2c68ed761c688f2f80e9bcfb8ddb17c2259e3
Vasiliy <60792301+VasiliyBologov@users.noreply.github.com>
2022-02-08 23:51:10 +0200

    add model

---
+++ models.py
@@ -0,0 +1,13 @@
+import mongoengine as me
+
+
+class Book(me.Document):
+    name = me.StringField(required=True)
+    author = me.StringField(required=True)
+    publishing_house = me.StringField(required=True)
+    year = me.IntField(required=True)
+    # example isbn 978-5-4461-0846-6
+    isbn = me.StringField()
+    # example bbk 32.973 / 84P7 / 84(2Рус+Рус)6-44
+    bbk = me.StringField()
+    
